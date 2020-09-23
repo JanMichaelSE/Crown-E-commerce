739c85b5cffef79ec1b4bb85a1a52d41d93eb351
JanMichaelSe <janm.montalvo@gmail.com>
2020-09-23 18:53:32 -0400

    added compression for heroku in backend

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+import compression from 'compression';
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -10,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
