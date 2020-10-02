29ce0a2286cd84d6ce79e3288b1a9e8982b5e976
JanMichaelSe <janm.montalvo@gmail.com>
2020-10-01 20:25:56 -0400

    Finished MVP

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
